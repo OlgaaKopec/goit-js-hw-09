36c6ef26dee76c3992dbf4362159cbb1569a9961
OlgaaKopec <Olga.kopec@gmail.com>
2023-05-30 21:49:30 +0200

    try2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startButton = document.querySelector('[data-start]')
+const stopButton = document.querySelector('[data-stop]')
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+}
+
+let changeColor = null
+startButton.addEventListener('click', function() {
+    startButton.disabled = true;
+    changeColor = setInterval(function() {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  })
+
+  stopButton.addEventListener('click', function() {
+    startButton.disabled = false;
+    clearInterval(changeColor);
+  })
+
+  const website = document.createElement('div')
+  website.classList.add('website')
+  website.appendChild(startButton)
+  website.appendChild(stopButton)
+document.body.insertBefore(website, document.body.secondChild)
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+"use strict"
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const shouldResolve = Math.random() > 0.3;
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
   }
-}
+
+  const form = document.querySelector('.form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const delay = parseInt(event.target.elements.delay.value);
+    const step = parseInt(event.target.elements.step.value);
+    const amount = parseInt(event.target.elements.amount.value);
+
+    for (let i = 1; i <= amount; i++) {
+      const position = i;
+      const currentDelay = delay + (i - 1) * step;
+      createPromise(position, currentDelay)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
+  });
