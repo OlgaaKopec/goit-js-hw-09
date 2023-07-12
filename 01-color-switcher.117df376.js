252f6519bf8e4824dea25f0fb3254bf8fe641278
OlgaaKopec <117989251+OlgaaKopec@users.noreply.github.com>
2023-07-12 16:47:47 +0000

    Deploying to gh-pages from @ OlgaaKopec/goit-js-hw-09@2768169774803fe207daf571ed8950d888b504e5 🚀

---
+++ 01-color-switcher.117df376.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;e.addEventListener("click",(function(){e.disabled=!0,d=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(function(){e.disabled=!1,clearInterval(d)}));const n=document.createElement("div");n.classList.add("website"),n.appendChild(e),n.appendChild(t),document.body.insertBefore(n,document.body.secondChild);
+//# sourceMappingURL=01-color-switcher.117df376.js.map
